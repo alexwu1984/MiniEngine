71707a8ee8a5ae0d5c0cc71f963bd2c0af1b4b35
alexwu <alexwu1984@126.com>
2023-10-11 23:26:10 +0800

    添加模型显示

--- Engine/Src/Engine/GltfModel/GltfModelConfig.cpp
+++ Engine/Src/Engine/GltfModel/GltfModelConfig.cpp
@@ -50,29 +50,37 @@ namespace Engine
 		}
 		d->ModelName = core::u8_ucs2(GltfJson["Model"]);
 
-		auto DyBoneListJson = d->Config["DyBone"];
-
-		for (auto Item: DyBoneListJson)
+		try
 		{
-			DynamicBoneInfo BoneInfo;
-			BoneInfo.BoneName = Item["Name"];
-			BoneInfo.Damping = Item["Damping"].get<float>();
-			BoneInfo.Elasticity = Item["Elasticity"].get<float>();
-			BoneInfo.Stiffness = Item["Stiffness"].get<float>();
-			BoneInfo.Inert = Item["Inert"].get<float>();
-			d->DyBonelist.push_back(BoneInfo);
+			auto DyBoneListJson = d->Config["DyBone"];
+
+			for (auto Item : DyBoneListJson)
+			{
+				DynamicBoneInfo BoneInfo;
+				BoneInfo.BoneName = Item["Name"];
+				BoneInfo.Damping = Item["Damping"].get<float>();
+				BoneInfo.Elasticity = Item["Elasticity"].get<float>();
+				BoneInfo.Stiffness = Item["Stiffness"].get<float>();
+				BoneInfo.Inert = Item["Inert"].get<float>();
+				d->DyBonelist.push_back(BoneInfo);
+			}
+
+			auto FurJson = d->Config["FurMaterial"];
+			d->FurConfig.Name = FurJson["Name"];
+			d->FurConfig.NoiseTex = FurJson["NoiseTex"];
+			d->FurConfig.FurLength = FurJson["FurLength"];
+			d->FurConfig.FurAmbientStrength = FurJson["FurAmbientStrength"];
+			d->FurConfig.FurLevel = FurJson["FurLevel"];
+			d->FurConfig.UVScale = FurJson["UVScale"];
+			d->FurConfig.FurLightExposure = FurJson["FurLightExposure"];
+			std::string Gravity = FurJson["Gravity"];
+			sscanf_s(Gravity.c_str(), "%f,%f,%f", &d->FurConfig.Gravity.x, &d->FurConfig.Gravity.y, &d->FurConfig.Gravity.z);
+		}
+		catch (const std::exception&)
+		{
+
 		}
 
-		auto FurJson = d->Config["FurMaterial"];
-		d->FurConfig.Name = FurJson["Name"];
-		d->FurConfig.NoiseTex = FurJson["NoiseTex"];
-		d->FurConfig.FurLength = FurJson["FurLength"];
-		d->FurConfig.FurAmbientStrength = FurJson["FurAmbientStrength"];
-		d->FurConfig.FurLevel = FurJson["FurLevel"];
-		d->FurConfig.UVScale = FurJson["UVScale"];
-		d->FurConfig.FurLightExposure = FurJson["FurLightExposure"];
-		std::string Gravity = FurJson["Gravity"];
-		sscanf_s(Gravity.c_str(), "%f,%f,%f", &d->FurConfig.Gravity.x, &d->FurConfig.Gravity.y, &d->FurConfig.Gravity.z);
 		return true;
 	}
 
